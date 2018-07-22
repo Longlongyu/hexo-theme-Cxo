b9e6beba63ea9be40f3f7c4e6a7387a057b81d89
Longlongyu <longlongyuu@gmail.com>
2018-07-22 21:46:08 +0800

    fixed: fix some bug about Edge.

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,8 +1,8 @@
 export default () => {
   document.querySelectorAll('.recent-post-content img,.article-entry img').forEach(img => {
     let outer = img.outerHTML
-    let imgSrc = /src="(.*)"/.exec(outer) && /src="(.*)"/.exec(outer)[1]
-    let imgAlt = /alt="(.*)"/.exec(outer) && /alt="(.*)"/.exec(outer)[1]
+    let imgSrc = img.src
+    let imgAlt = img.alt
     img.outerHTML = `<a class="fancy-link" href="${imgSrc}" data-fancybox="group" data-caption="${imgAlt}">${outer}<div class="img-alt">${imgAlt}</div></a>`
   })
 }
